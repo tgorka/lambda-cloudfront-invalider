7d89c8957e392db7f342f0c258c0f44a37ca53d7
tgorka <tomasz.gorka@carmoov.io>
2019-07-22 14:57:22 +0100

    implementation

--- src/invalidate.ts
+++ src/invalidate.ts
@@ -1,13 +1,29 @@
 import * as _debug from "debug";
+import * as AWS from "aws-sdk";
 
 
 const info = _debug("cloudfrontinvalider-info-handler");
 const debug = _debug("cloudfrontinvalider-debug-handler");
 const error = _debug("cloudfrontinvalider-error-handler");
 
-export default async (id: string, path?: string): Promise<any> => {
+export default async (id: string, path?: string, invalidationId?: string): Promise<any> => {
     path = path || "/*";
+    invalidationId = invalidationId || "STRING_VALUE";
+    const parameters = {
+        DistributionId: id, /* required */
+        InvalidationBatch: { /* required */
+            CallerReference: invalidationId, /* required */
+            Paths: { /* required */
+                Quantity: 1, /* required */
+                Items: [
+                    path,
+                ]
+            }
+        }
+    };
+    const response = await new AWS.CloudFront({apiVersion: '2019-03-26'}).createInvalidation(parameters).promise();
 
     info(`cloudfront[${id}] created invalidation for:${path}.`);
-    return {success: true};
+    debug(`Invalidation response:${JSON.stringify(response)}`);
+    return response;
 };
